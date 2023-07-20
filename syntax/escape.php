3b00f61a1bfe2b85284ab1a2225a1a0910fb2488
Thiruvarasu-Datasirpi <thiruvarasu.u@datasirpi.com>
2023-07-20 09:55:15 +0530

    escape task

---
+++ syntax/escape.php
@@ -0,0 +1,28 @@
+<?php
+
+$a = "thiru is a \"php\" developer \n";
+$fPATH = "C:\\USER\\DESKTOP\\DOCUMENT \n";
+$americancurr = "10\n";
+$str = "hello,\n how are you?\n";
+$str1 = "hello,\r how are you?\n";
+$str2 = "name:\t Thiru\n";
+// page break
+$name = "thiru\fvarasu\n";
+
+$frstwrd = "Hello\0World\n";
+
+
+echo $a;
+echo "The path of the folder is $fPATH";
+echo "The amount is \$" . $americancurr;
+echo $str;
+echo $str1;
+echo $str2;
+echo $name;
+
+echo $frstwrd;
+
+
+
+
+?>

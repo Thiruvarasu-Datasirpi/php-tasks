5a64fae88181af4222c1586cdab3e0e836be4c2d
Thiruvarasu-Datasirpi <138755453+Thiruvarasu-Datasirpi@users.noreply.github.com>
2023-07-21 10:04:44 +0530

    constants in php

---
+++ constants/constants.php
@@ -0,0 +1,16 @@
+
+<?php
+class Math {
+    const PI = 3.14;
+}
+
+echo Math::PI."\n"; 
+
+
+echo __FILE__."\n"; 
+echo __LINE__."\n"; 
+define("MY_CONSTANT", "Hello, World!");
+const PI = 3.14;
+
+echo MY_CONSTANT."\n";
+?>

e6d1935e75251801bc407838c359c94ec130b862
Thiruvarasu-Datasirpi <138755453+Thiruvarasu-Datasirpi@users.noreply.github.com>
2023-07-24 10:50:50 +0530

    include()method

---
+++ include/inc.php
@@ -0,0 +1,11 @@
+<?php
+function add ($num1,$num2){
+    $result=$num1+$num2;
+    echo $result."\n";
+
+}
+function msg($name1,$name2){
+    $fullname=$name1." ".$name2;
+    echo $fullname;
+}
+?>
---
+++ include/incs.php
@@ -0,0 +1,9 @@
+<?php
+include 'inc.php';
+
+add(10,20);
+
+msg("thiru","varasu");
+
+
+?>

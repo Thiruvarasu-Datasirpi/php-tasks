de43af89daa44ea5a6e57f0d1206c2b8a1e9054b
Thiruvarasu-Datasirpi <138755453+Thiruvarasu-Datasirpi@users.noreply.github.com>
2023-08-08 09:36:03 +0530

    variables in dart

---
+++ vars.dart
@@ -0,0 +1,22 @@
+
+void main() {
+  String name = "Dart";
+  int age = 20;
+  double height = 1.70;
+  int weight = 60;
+  print(name);
+  print(age);
+  print(height);
+  print(weight);
+ 
+  if (age > 18) {
+    print("You are old enough to vote.");
+  } else {
+    print("You are not old enough to vote.");
+  }
+  if (name == "Dart") {
+    print("Your name is Dart.");
+  } else {
+    print("Your name is not Dart.");
+  }
+}
